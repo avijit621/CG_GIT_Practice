6e9b0b9131d171cf9672fd90e69d4c028e596a71
avijit621 <avijit621@gmail.com>
2022-09-27 15:34:40 +0530

    Adding Deopendencies

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EMS;
 namespace HelloWorld
 {
     internal class Program
@@ -11,6 +11,8 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Today is Tuesday");
+            Employee employee = new Employee { ID=20,Name="Tom",Salary=20000};
         }
     }
 }
